43c9d83e525e6241c0065853e28a112520bd9104
olucaslopesx <lucasalvarengalopes@gmail.com>
2024-07-30 14:53:27 -0300

    Screen match modulo 4

---
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -0,0 +1,42 @@
+package br.com.alura.screenmatch.service;
+
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+	@Autowired
+	private SerieRepository serieRepository;
+	
+	public List<SerieDTO> getAll() {
+		return listToDTO(serieRepository.findAll());
+	}
+	
+	private List<SerieDTO> listToDTO(List<Serie> series) {
+		return series.stream().map(this::toDTO).collect(Collectors.toList());
+	}
+	
+	private SerieDTO toDTO(Serie series) {
+		return new SerieDTO(series.getId(), series.getTitulo(), series.getTotalTemporadas(), series.getAvaliacao(), series.getGenero(), series.getAtores(), series.getPoster(), series.getSinopse());
+	}
+	
+	public List<SerieDTO> getTop5() {
+		return listToDTO(serieRepository.findTop5ByOrderByAvaliacaoDesc());
+	}
+	
+	public SerieDTO get(Long id) {
+		Optional<Serie> s = serieRepository.findById(id);
+		return s.map(this::toDTO).orElse(null);
+	}
+	
+	public List<SerieDTO> getReleases() {
+		return listToDTO(serieRepository.lancamentosRecentes());
+	}
+}
